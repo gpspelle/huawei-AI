df02127b3aa4fcf6fe871512c22c9323c01356b4
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2020-11-10 13:51:11 +0100

    working xgburro_metadata

--- submissions/xgburro_metadata/regressor.py
+++ submissions/xgburro_metadata/regressor.py
@@ -17,17 +17,24 @@ class Regressor():
             metadata_input = [0] * 8 * 3
             for i, j in zip(range(8), range(0, 24, 3)):
                 if i >= len(metadata):
-                    metadata_input[j:j+2] = [0, 0, 0]
+                    metadata_input[j] = 0
+                    metadata_input[j+1] = 0
+                    metadata_input[j+2] = 0
                 else:
                     metadata_input[j] = 2 if metadata[i][0] == 'EDFA' else 1
-                    metadata_input[j+1] = metadata[i][1][0]
-                    metadata_input[j+2] = metadata[i][1][1]
+                    try:
+                        metadata_input[j+1] = metadata[i][1][0]
+                    except:
+                        metadata_input[j+1] = 0
+                    try:
+                        metadata_input[j+2] = metadata[i][1][1]
+                    except:
+                        metadata_input[j+2] = 0
 
-            X_56.append(np.asarray(inp + metadata_input))
+            real_inp = inp + metadata_input
+            X_56.append(np.asarray(real_inp))
 
         X_56 = np.asarray(X_56)
-        print(X_56[0])
-        print(X_56[0].shape)
         self.model = multioutput.MultiOutputRegressor(xgb.XGBRegressor()).fit(X_56, y)
 
     def predict(self, X):
@@ -38,13 +45,26 @@ class Regressor():
             inp = sample[1]
 
             metadata_input = [0] * 8 * 3
-            for i, j in zip(range(8), range(1, 24, 3)):
+            for i, j in zip(range(8), range(0, 24, 3)):
                 if i >= len(metadata):
-                    metadata_input[j:j+2] = [0, 0, 0]
+                    metadata_input[j] = 0
+                    metadata_input[j+1] = 0
+                    metadata_input[j+2] = 0
                 else:
-                    metadata_input[j] = 2 if metadata[i][0] == 'EDFA' else 1
-                    metadata_input[j+1] = metadata[i][1][0]
-                    metadata_input[j+2] = metadata[i][1][1]
+                    try:
+                        metadata_input[j] = 2 if metadata[i][0] == 'EDFA' else 1
+                    except:
+                        metadata_input[j] = 0 
+                    try:
+                        metadata_input[j+1] = metadata[i][1][0]
+                    except:
+                        metadata_input[j+1] = 0
+                    try:
+                        metadata_input[j+2] = metadata[i][1][1]
+                    except:
+                        metadata_input[j+2] = 0
+
+
 
             X_56.append(inp + metadata_input)
 
